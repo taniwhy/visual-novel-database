6e84998479a70308a1da6f73df914a1d34290a60
taniwhy <tanitani628@gmail.com>
2020-03-25 11:45:48 +0900

    init

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,28 +1,29 @@
-DROP TABLE IF EXISTS blandlist CASCADE;
-DROP TABLE IF EXISTS blandlist_id_seq CASCADE;
+DROP TABLE IF EXISTS blandlists CASCADE;
+DROP TABLE IF EXISTS blandlists_id_seq CASCADE;
 
 CREATE TABLE blandlists
 (
-    id SERIAL,
-    bland_name VARCHAR(255) NOT NULL,
-    bland_furigana VARCHAR(255) NOT NULL,
+    id              SERIAL,
+    bland_name      TEXT    NOT NULL,
+    bland_furigana  TEXT    NOT NULL,
+    url             TEXT,
+    twitter_id      TEXT,
     PRIMARY KEY(id)
 );
 
-DROP TABLE IF EXISTS gamelist;
-DROP TABLE IF EXISTS gamelist_id_seq;
+DROP TABLE IF EXISTS gamelists;
+DROP TABLE IF EXISTS gamelists_id_seq;
 
-CREATE TABLE gamelist
+CREATE TABLE gamelists
 (
     id SERIAL,
-    game_name TEXT NOT NULL,
-    game_furigana TEXT NOT NULL,
-    bland_name INTEGER NOT NULL,
-    launch_day DATE NOT NULL,
-    
-    platform TEXT NOT NULL,
+    game_name       TEXT    NOT NULL,
+    game_furigana   TEXT    NOT NULL,
+    bland_name      INTEGER NOT NULL,
+    launch_day      DATE    NOT NULL,
+    platform        TEXT    NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(bland_name) REFERENCES blandlist(id) 
+    FOREIGN KEY(bland_name) REFERENCES blandlists(id) 
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
